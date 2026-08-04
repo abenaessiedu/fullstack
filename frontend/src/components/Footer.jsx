9f0929d008135e6e7a77757585eed5f562925fad
abenaessidedu <aessiedu98@gmail.com>
2026-08-04 10:02:10 -0400

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+
+export default function Footer() {
+
+    return (
+        <footer className="footer"> 
+        <Nav type="footer" /> 
+        <p>&copy; </p>
+        
+        
+        </footer>
+    
+    ); 
+}; 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header className="header">
+        <h1>
+            
+
+        </h1>
+        <h2>
+            Better-Dressed People
+
+        </h2>
+
+    </header>
+
+    ); 
+}
