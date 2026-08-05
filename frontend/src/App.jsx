33888870c9ba5f378a98bd6fb820ab4848b181b8
abenaessidedu <aessiedu98@gmail.com>
2026-08-05 09:10:08 -0400

    added images to landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,43 @@ import React from "react";
 export default function App() {
   return (
     <main>
-      <header> 
-        
+      <header>
+
         <a href="" className="logo"> MYBLOG </a>
-        <nav> 
+        <nav>
           <a href=""> LOGIN </a>
           <a href=""> REGISTER </a>
         </nav>
       </header>
-        <div className="post"> 
-          <img src="" alt="" /> 
-          <h2> BEAUTY BY AD. </h2>
-          <p> by Adeola (Chizoba) Adeyemi, 2017, Nigeria </p>
-
+      <div className="post">
+        <img src="/BEAUTYBYAD.png" alt="BEAUTYBYAD logo" />
+        <div className="texts"> 
+        <h2> BEAUTY BY AD. </h2>
+        <p> by Adeola (Chizoba) Adeyemi, 2017, Nigeria </p>
         </div>
-          <div className="post"> 
-          <img src="" alt="" /> 
-          <h2> DANESSA MYRICKS BEAUTY </h2>
-          <p>  </p>
 
-        </div>
-    
-    
+      </div>
+      <div className="post">
+        <img src="/DANESSAMYRICKSBEAUTY.png" alt="DANESSAMYRICKSBEAUTY logo" />
+        <div className="texts"> 
+        <h2> DANESSA MYRICKS BEAUTY </h2>
+        <p> by Danessa Myricks, 2005, USA </p>
+        </div> 
+
+      </div>
+
+      <div className="post">
+        <img src="/PATMCGRATHLABS.png" alt="PATMCGRATHLABS logo" />
+        <div className="texts"> 
+        <h2> PAT MCGRATH LABS </h2>
+        <p> by Pat McGrath, 2015, UK </p>
+        </div> 
+
+      </div>
+
+
+
+
     </main>
-  ); 
+  );
 }
