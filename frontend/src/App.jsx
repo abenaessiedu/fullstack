fcc989b9b87ed5fb43b59b7ef9b412f0a642a77b
abenaessidedu <aessiedu98@gmail.com>
2026-08-05 13:57:51 -0400

    styling and reworking some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-// import BrandCard from "./components/BrandCard"
-// import Header from "./components/Header";
+import './App.css'; 
+import BrandCard from "./components/BrandCard"
+import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout"
+import Layout from "./components/Layout"; 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-export default function App() {
+function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}> 
        
-       <Route index element={<Home /> } />
+       <Route index element={<Home />} />
       <Route path="/login" element={<Login />} /> 
       <Route path="/register" element={<Register />} />
       </Route>
@@ -20,3 +21,4 @@ export default function App() {
 
   );
 }
+export default App; 
--- frontend/src/components/BrandCard.jsx
+++ frontend/src/components/BrandCard.jsx
@@ -1,49 +1,21 @@
 export default function BrandCard() {
     return (
-        <>
-            <div className="post">
+        
+            <div className="BrandCard">
                 <div className="image">
                     <img src="/BEAUTYBYAD.png" alt="BEAUTYBYAD logo" />
                 </div>
+
                 <div className="texts">
                     <h2> BEAUTY BY AD. </h2>
                     <p className="info">
                         <a className="author"> Adeola Adeyemi </a>
                         <time> 2026-08-05 9:23 </time>
                     </p>
+
                     <p className="summary"> by Adeola (Chizoba) Adeyemi, 2017, Nigeria </p>
                 </div>
 
             </div>
-            <div className="post">
-                <div className="image">
-                    <img src="/DANESSAMYRICKSBEAUTY.png" alt="DANESSAMYRICKSBEAUTY logo" />
-                </div>
-                <div className="texts">
-                    <h2> DANESSA MYRICKS BEAUTY </h2>
-                    <p className="info">
-                        <a className="author"> Danessa Myricks  </a>
-                        <time> 2026-08-05 9:23 </time>
-                    </p>
-                    <p> by Danessa Myricks, 2005, USA </p>
-                </div>
-
-            </div>
-
-            <div className="post">
-                <div className="image">
-                    <img src="/PATMCGRATHLABS.png" alt="PATMCGRATHLABS logo" />
-                </div>
-                <div className="texts">
-                    <h2> PAT MCGRATH LABS </h2>
-                    <p className="info">
-                        <a className="author"> Adeola Adeyemi </a>
-                        <time> 2026-08-05 9:23 </time>
-                    </p>
-                    <p> by Pat McGrath, 2015, UK </p>
-                </div>
-
-            </div>
-        </>
-    )
+    ); 
 }
